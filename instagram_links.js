eefe3b40a1da5c74315bf4528a5033fb939e45da
kleinlabyale <dk698@connect.yale.edu>
2026-02-20 19:05:48 -0600

    Add files via upload

---
+++ instagram_links.js
@@ -0,0 +1,30 @@
+// Edit only this file to update the Instagram section.
+// Keep one object per post with:
+// - postUrl: Instagram post/reel URL
+// - caption: text shown on hover
+window.instagramFeedItems = [
+  {
+    postUrl: "https://www.instagram.com/p/DUnvn9fEUbN/",
+    caption: "We've come a long way!"
+  },
+  {
+    postUrl: "https://www.instagram.com/p/DUq5XNLkdUk/",
+    caption: "ASBMB Chicago. JADEK runs the Lakefront!"
+  },  
+  {
+    postUrl: "https://www.instagram.com/p/DUl9UfekfYV/",
+    caption: "Beam on!"
+  },
+  {
+    postUrl: "https://www.instagram.com/p/DUl9DKkEdvm/",
+    caption: "Find us here"
+  },
+  {
+    postUrl: "https://www.instagram.com/p/DUl845SEZTl/",
+    caption: "Thanks Yale Ventures!"
+  },
+  {
+    postUrl: "https://www.instagram.com/p/DUl1VMikeV9/",
+    caption: "Science reveals secrets hidden in plain sight"
+  }
+];
